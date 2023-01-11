9680164d5ac7161130103165ba2b50cda8a27ff5
perkles <christian.fdacr@gmail.com>
2023-01-11 20:58:06 -0300

    Project created

---
+++ backend/src/main/java/com/ControleDeVendasVendedor/cadastroDeVendedor/CadastroDeVendedorApplication.java
@@ -0,0 +1,13 @@
+package com.ControleDeVendasVendedor.cadastroDeVendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeVendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeVendedorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ControleDeVendasVendedor/cadastroDeVendedor/CadastroDeVendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControleDeVendasVendedor.cadastroDeVendedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeVendedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
